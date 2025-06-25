d3a8480a468b5d5b90c4cfa81551de0a8adc33e5
Tohir78 <tohir78marketing@gmail.com>
2025-06-26 01:53:13 +0700

    Update App.tsx title

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Tohir78 Game Online, Slot, dan Lotto Terpopuler 2025</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
